35098836f322f61da8680ef21d19f9d8ffa08cca
Dave Tamayo <djbtamayo@gmail.com>
2017-01-02 22:46:27 -0500

    Started working on the Shop functionality

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class PlayerData : MonoBehaviour {
+
+	int currentPower;
+
+
+	void Awake () {
+
+		currentPower = 3000;
+	}
+
+
+	#region Getters
+	public int GetCurrentPower () {
+
+		return currentPower;
+	}
+
+	public void ChangeCurrentPower (int amount) {
+
+		currentPower += amount;
+	}
+	#endregion
+}
